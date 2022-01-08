5c16a74a072c570cba375f01cc0707b3d731a8b8
dmitryjob4j <haoos@inbox.ru>
2022-01-09 00:19:15 +0300

    2.3.6.Проект.Агрегатор Java вакансий.4.Схема таблицы Post[#1731].

--- db/scripts/schema.sql
+++ db/scripts/schema.sql
@@ -1,7 +1,7 @@
 create table if not exists post(
 	id serial primary key,
-	name text,
-	link text unique,
+	name varchar(255) not null,
+	link varchar(255) unique not null,
 	text text,
 	created timestamp
 );
